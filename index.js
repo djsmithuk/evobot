187e38cdd2c07b13f464d07b0b8b17223a20597e
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-10-26 23:25:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'NzA0NjczMTIyODI1MDExMjcw.Xqgj_A.Gk3_5SI0Bmdsef9DWyNBZXM2pzg' });
+const manager = new ShardingManager('./bot.js', { token: 'NzA0NjczMTIyODI1MDExMjcw.Xqgj_A.To7TIsirSb-P5Me5wEwnNXoeo0s' });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
