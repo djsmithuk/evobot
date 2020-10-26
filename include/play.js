32484215e9bbf054e7eab7f1b81abd35316e9202
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-10-26 20:06:33 +0000

    Update play.js

--- include/play.js
+++ include/play.js
@@ -24,12 +24,12 @@ module.exports = {
 
     let stream = null;
     //let streamType = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
-    let streamType = song.url.startsWith('https://youtube.com/')? "opus" : "ogg/opus";
+    let streamType = song.url.includes('https://youtube.com/')? "opus" : "ogg/opus";
 
     try {
-      if (song.url.startsWith("https://youtube.com/")) {
+      if (song.url.includes("https://youtube.com/")) {
         stream = await ytdlDiscord(song.url, { highWaterMark: 1 << 25 });
-      } else if (song.url.startsWith("https://soundcloud.com/")) {
+      } else if (song.url.includes("https://soundcloud.com/")) {
         try {
           stream = await scdl.downloadFormat(
             song.url,
