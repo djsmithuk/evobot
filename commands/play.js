9fe50ecfc7d6f40819d9c5a51f666f2171fde924
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-10-27 15:43:50 +0000

    updated definition

--- commands/play.js
+++ commands/play.js
@@ -18,7 +18,7 @@ module.exports = {
   name: "play",
   cooldown: 3,
   aliases: ["p"],
-  description: "Plays audio from YouTube or Soundcloud",
+  description: "Plays a song from YouTube or Soundcloud",
   async execute(message, args) {
     const { channel } = message.member.voice;
 
