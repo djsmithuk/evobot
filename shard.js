4944e08cb3c605437f0480b09a2724def3da45b0
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-11-09 22:17:29 +0000

    sharding changes

--- shard.js
+++ shard.js
@@ -1,6 +1,13 @@
-const config = require("./config.json");
+
 const { ShardingManager } = require('discord.js');
 
+let TOKEN, PREFIX;
+try {
+  const config = require("./config.json");
+  TOKEN = config.TOKEN;
+} catch (error) {
+  TOKEN = process.env.TOKEN;
+}
 
 const manager = new ShardingManager('./index.js', {
     totalShards: 'auto',
