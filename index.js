aa9f1ee1275638aabfc80ee74aeec3f1e35ea455
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 16:57:41 +0000

    reverted folder structure

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on("error", console.error);
 /**
  * Import all commands
  */
-const commandFiles = readdirSync(join(__dirname, "commands" && "commands/general" && "commands/music" )).filter((file) => file.endsWith(".js"));
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
   const command = require(join(__dirname, "commands", `${file}`));
   client.commands.set(command.name, command);
