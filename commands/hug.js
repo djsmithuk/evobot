66a73014b089070b5af94fc87c07003a5e8bc7e0
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 17:34:47 +0000

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, Message } = require('discord.js');
+/*const { MessageEmbed, Message } = require('discord.js');
 
 module.exports = {
     name: "hug",
@@ -17,3 +17,21 @@ module.exports = {
         }
     }
 }
+*/
+
+module.exports = {
+    name: 'hug',
+    description: 'Used to hug everyone or mention a user to hug them specifically.',
+    execute(message,args) {
+        args = message.mentions.users.size;
+
+        if (!args) {
+            message.delete();
+            return message.channel.send(`${message.author} gives a big ol' hug to everyone!`);
+        }
+        else {
+            message.delete();
+            return message.channel.send(`${message.author} sends love to ${message.mentions.users.first()} with a hug!`);
+        }
+    },
+};
