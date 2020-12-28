fb5dc46a639e9dafa916c8b11a1f7f01433db0e4
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 17:32:34 +0000

    stripped embed

--- commands/hug.js
+++ commands/hug.js
@@ -5,13 +5,13 @@ module.exports = {
     description: "Hug A Member",
     execute(message) {
         const target = message.mentions.users.first()
-        if (!target) return message.channel.send(`<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(`)
+        if (!target) return message.channel.send("<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(")
         if (target.id === message.author.id) {
             let hugEmbed = new MessageEmbed()
-                .setTitle('Get Hugged Kiddo')
-                .setImage('https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif')
-                .setColor('#7289da')
-                .setFooter(`@${message.author.username} Hugged @${target.username}`)
+                .setTitle("Get Hugged Kiddo")
+                .setImage("https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif")
+                .setColor("#7289da")
+                .setFooter("@${message.author.username} Hugged @${target.username}")
 
             return message.channel.send(hugEmbed).catch(console.error);
         }
