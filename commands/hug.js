de5da2c6e73e5bd69f56853c41527672951aad2d
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 17:13:55 +0000

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -3,17 +3,17 @@ const { MessageEmbed, Message } = require('discord.js');
 module.exports = {
     name: 'hug',
     description: 'Hug A Member',
-    run: async (client, message) => {
+    execute(message) {
         let target = message.mentions.users.first()
         if (!target) return message.channel.send(`<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(`)
         if (target.id === message.author.id) {
-            const embed = new MessageEmbed()
+            let hugEmbed = new MessageEmbed()
                 .setTitle('Get Hugged Kiddo')
                 .setImage('https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif')
                 .setColor('#7289da')
                 .setFooter(`@${message.author.username} Hugged @${target.username}`)
 
-            message.channel.send(embed)
+            return message.channel.send(hugEmbed).catch(console.error);
         }
     }
 }
