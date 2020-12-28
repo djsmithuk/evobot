a2412da2e934308cbc58925226716778ab258c92
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 17:37:33 +0000

    added gif

--- commands/hug.js
+++ commands/hug.js
@@ -27,11 +27,11 @@ module.exports = {
 
         if (!args) {
             message.delete();
-            return message.channel.send(`${message.author} gives a big ol' hug to everyone!`);
+            return message.channel.send(`${message.author} gives a big ol' hug to everyone!\n https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif`);
         }
         else {
             message.delete();
-            return message.channel.send(`${message.author} sends love to ${message.mentions.users.first()} with a hug!`);
+            return message.channel.send(`${message.author} sends love to ${message.mentions.users.first()} with a hug!\n https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif`);
         }
     },
 };
