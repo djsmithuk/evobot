bf17a5bb9ff26085755a8013f7a93946b03b8417
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 17:44:23 +0000

    basic embed

--- commands/hug.js
+++ commands/hug.js
@@ -1,24 +1,30 @@
-/*const { MessageEmbed, Message } = require('discord.js');
+/*const { MessageEmbed, Message } = require('discord.js');*/
 
 module.exports = {
-    name: "hug",
-    description: "Hug A Member",
-    execute(message) {
-        const target = message.mentions.users.first()
-        if (!target) return message.channel.send("<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(")
-        if (target.id === message.author.id) {
-            let hugEmbed = new MessageEmbed()
-                .setTitle("Get Hugged Kiddo")
-                .setImage("https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif")
-                .setColor("#7289da")
-                .setFooter("@${message.author.username} Hugged @${target.username}")
+  name: 'hug',
+  description: 'Used to hug everyone or mention a user to hug them specifically.',
+  execute(message,args) {
+    args = message.mentions.users.size;
 
-            return message.channel.send(hugEmbed).catch(console.error);
-        }
+    if (!args) {
+      message.delete();
+      return message.channel.send("<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(")
     }
+    else{
+      message.delete();
+      let hugEmbed = new MessageEmbed()
+          .setTitle("Get Hugged Kiddo")
+          .setImage("https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif")
+          .setColor("#7289da")
+          .setFooter('@${message.author.username} Hugged @${target.username}')
+      return message.channel.send(hugEmbed).catch(console.error();
+    }
+  }
 }
 */
 
+
+/* WORKING VERSION
 module.exports = {
     name: 'hug',
     description: 'Used to hug everyone or mention a user to hug them specifically.',
@@ -27,11 +33,12 @@ module.exports = {
 
         if (!args) {
             message.delete();
-            return message.channel.send(`${message.author} gives a big ol' hug to everyone!\n https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif`);
+            return message.channel.send(`${message.author} gives a big ol' hug to everyone!`);
         }
         else {
             message.delete();
-            return message.channel.send(`${message.author} sends love to ${message.mentions.users.first()} with a hug!\n https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif`);
+            return message.channel.send(`${message.author} sends love to ${message.mentions.users.first()} with a hug!`);
         }
     },
 };
+*/
