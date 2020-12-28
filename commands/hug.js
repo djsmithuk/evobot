90a3c9caa1549df4eae5a30f6f4e00e0aa67f5fb
Daniel Smith <33575896+djsmithuk@users.noreply.github.com>
2020-12-28 17:09:29 +0000

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,20 +1,19 @@
 const { MessageEmbed, Message } = require('discord.js');
 
 module.exports = {
-  name: "hug",
-  description: "Hug a user.",
+    name: 'hug',
+    description: 'Hug A Member',
+    run: async (client, message) => {
+        let target = message.mentions.users.first()
+        if (!target) return message.channel.send(`<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(`)
+        if (target.id === message.author.id) {
+            const embed = new MessageEmbed()
+                .setTitle('Get Hugged Kiddo')
+                .setImage('https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif')
+                .setColor('#7289da')
+                .setFooter(`@${message.author.username} Hugged @${target.username}`)
 
-  execute(message) {
-
-    let target = message.mentions.users.first()
-    if (!target) return message.channel.send(`<@${message.author.id}>! Please Tag A User To Hug Or Tag Yourself To Hug Yourself lol ;(`)
-    if (target.id === message.author.id) {
-        const embed = new MessageEmbed()
-            .setTitle('Get Hugged Kiddo')
-            .setImage('https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif')
-            .setColor('#7289da')
-            .setFooter(`@${message.author.username} Hugged @${target.username}`)
-
-        message.channel.send(embed)
-  }
+            message.channel.send(embed)
+        }
+    }
 }
